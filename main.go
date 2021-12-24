e910a7537f12cd1cc72a116bb4d7128658cb8d2c
dani rosyadi <dani.rosyadi@gojek.com>
2021-12-24 09:37:17 +0700

    create url shortener

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Printf("Hello Go URL Shortener !🚀")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey Go URL Shortener !",
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+}
